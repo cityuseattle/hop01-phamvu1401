f3e1ed691baceddfedfbfa13ce6f274bfdf87aa6
Vu Pham <38738288+phamvu1401@users.noreply.github.com>
2020-11-02 16:18:28 -0800

    Add files via upload

---
+++ HOP04/NDdnorm.R
@@ -0,0 +1,8 @@
+#find the value of the standard normal distribution pdf at x=1
+print(dnorm(x=1, mean=0, sd=1))
+
+#by default, R uses mean=0 and sd=1
+print(dnorm(x=1))
+
+#find the value of the normal distribution pdf at x=10 with mean=20 and sd=5
+print(dnorm(x=10, mean=20, sd=5))
